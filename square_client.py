a306cdd314d6047ba90b546ee5eee1bc93ce20c9
Subash karki <karki011@gmail.com>
2023-06-12 16:38:13 -0400

    update env variable

--- square_client.py
+++ square_client.py
@@ -1,6 +1,8 @@
 import os
 from square.client import Client
 
+print(f"Square Access Token: {os.environ.get('environment')}")
+
 access_token = os.environ.get('SQUARE_ACCESS_TOKEN')
 environment = os.environ.get('SQUARE_ENVIRONMENT')
 
