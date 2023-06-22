61fa3b9dc44c173ef31e7401c73b26a7a7a0b4dd
Subash karki <karki011@gmail.com>
2023-06-21 22:28:41 -0400

    refactor(square_client.py): use environment variables for Square API access token and environment

--- square_client.py
+++ square_client.py
@@ -6,6 +6,6 @@ access_token = os.environ.get('SQUARE_ACCESS_TOKEN')
 environment = os.environ.get('SQUARE_ENVIRONMENT')
 
 client = Client(
-    access_token='EAAAEQzgzmAXkoxcL2KURsjZPf8aEOKyRz3x9X5_bcgZv1ABEowTeY9nEqyTxg3Q',
-    environment='production'
+    access_token= access_token,
+    environment= environment
 )
