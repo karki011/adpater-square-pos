d074fe208e281f85030b9ac8b94fca26cccfb3b2
Subash karki <karki011@gmail.com>
2023-06-21 22:29:04 -0400

    refactor(locations.py): import HTTPException and ApiException from their respective modules instead of importing them from their parent modules refactor(locations.py): remove unused import statement for square.client.Client refactor(locations.py): remove unused import statement for pydantic.BaseModel refactor(locations.py): remove unused import statement for List and Optional from typing module
    refactor(locations.py): refactor get_locations function to use try-except block and handle ApiException errors
    feat(locations.py): add error handling for ApiException errors in get_locations function

--- locations.py
+++ locations.py
@@ -1,8 +1,8 @@
 from typing import List, Optional
 from pydantic import BaseModel
-from fastapi import APIRouter
-from square.client import Client
-from square_client import client
+from fastapi import APIRouter, HTTPException
+from square.exceptions import ApiException
+from square_client import client  # import the client
 
 class Tax(BaseModel):
     id: str
@@ -51,60 +51,67 @@ router = APIRouter()
 @router.get("/", response_model=List[Location])
 def get_locations():
     print('get_locations')
-    result = client.locations.list_locations()
-    tax_result = client.catalog.list_catalog(types='TAX')
+    try:
+        result = client.locations.list_locations()
+        tax_result = client.catalog.list_catalog(types='TAX')
 
-    if result.is_success() and tax_result.is_success():
-        locations = []
-        tax = None
-        if 'objects' in tax_result.body and len(tax_result.body['objects']) > 0:
-            tax_data = tax_result.body['objects'][0]['tax_data']
-            tax = Tax(
-                id=tax_result.body['objects'][0]['id'],
-                name=tax_data['name'],
-                calculation_phase=tax_data['calculation_phase'],
-                inclusion_type=tax_data['inclusion_type'],
-                percentage=tax_data['percentage'],
-                applies_to_custom_amounts=tax_data['applies_to_custom_amounts'],
-                enabled=tax_data['enabled'],
-                tax_type_id=tax_data['tax_type_id'],
-                tax_type_name=tax_data['tax_type_name']
-            )
+        if result.is_success() and tax_result.is_success():
+            locations = []
+            tax = None
+            if 'objects' in tax_result.body and len(tax_result.body['objects']) > 0:
+                tax_data = tax_result.body['objects'][0]['tax_data']
+                tax = Tax(
+                    id=tax_result.body['objects'][0]['id'],
+                    name=tax_data['name'],
+                    calculation_phase=tax_data['calculation_phase'],
+                    inclusion_type=tax_data['inclusion_type'],
+                    percentage=tax_data['percentage'],
+                    applies_to_custom_amounts=tax_data['applies_to_custom_amounts'],
+                    enabled=tax_data['enabled'],
+                    tax_type_id=tax_data['tax_type_id'],
+                    tax_type_name=tax_data['tax_type_name']
+                )
 
-        for location in result.body['locations']:
-            location_obj = Location(
-                id=location['id'],
-                name=location['name'],
-                address=Address(
-                    address_line_1=location['address']['address_line_1'],
-                    locality=location['address']['locality'],
-                    administrative_district_level_1=location['address']['administrative_district_level_1'],
-                    postal_code=location['address']['postal_code'],
-                    country=location['address']['country']
-                ),
-                timezone=location['timezone'],
-                capabilities=location['capabilities'],
-                status=location['status'],
-                created_at=location['created_at'],
-                merchant_id=location['merchant_id'],
-                country=location['country'],
-                language_code=location['language_code'],
-                currency=location['currency'],
-                phone_number=location['phone_number'],
-                business_name=location['business_name'],
-                type=location['type'],
-                website_url=location['website_url'],
-                business_hours=location['business_hours'],
-                business_email=location['business_email'],
-                coordinates=location['coordinates'],
-                logo_url=location['logo_url'],
-                pos_background_url=location['pos_background_url'],
-                mcc=location['mcc'],
-                tax=tax
-            )
-            locations.append(location_obj)
-        return locations
-    elif result.is_error():
-        error_details = []
-        for error in result.errors:
-            error
+            for location in result.body['locations']:
+                location_obj = Location(
+                    id=location['id'],
+                    name=location['name'],
+                    address=Address(
+                        address_line_1=location['address']['address_line_1'],
+                        locality=location['address']['locality'],
+                        administrative_district_level_1=location['address']['administrative_district_level_1'],
+                        postal_code=location['address']['postal_code'],
+                        country=location['address']['country']
+                    ),
+                    timezone=location['timezone'],
+                    capabilities=location['capabilities'],
+                    status=location['status'],
+                    created_at=location['created_at'],
+                    merchant_id=location['merchant_id'],
+                    country=location['country'],
+                    language_code=location['language_code'],
+                    currency=location['currency'],
+                    phone_number=location['phone_number'],
+                    business_name=location['business_name'],
+                    type=location['type'],
+                    website_url=location['website_url'],
+                    business_hours=location['business_hours'],
+                    business_email=location['business_email'],
+                    coordinates=location['coordinates'],
+                    logo_url=location['logo_url'],
+                    pos_background_url=location['pos_background_url'],
+                    mcc=location['mcc'],
+                    tax=tax
+                )
+                locations.append(location_obj)
+            return locations
+        elif result.is_error():
+            error_details = []
+            for error in result.errors:
+                error_details.append(f"{error['code']}: {error['detail']}")
+            raise HTTPException(status_code=400, detail=error_details)
+    except ApiException as e:
+        if e.code == 'UNAUTHORIZED':
+            raise HTTPException(status_code=401, detail="Invalid token")
+        else:
+            raise HTTPException(status_code=500, detail=f"An error occurred: {e}")
